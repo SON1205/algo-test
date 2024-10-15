1548e635cf1bf69ff0cd83edba79a863d6a3e5ea
SON1205 <sonjh970226@gmail.com>
2024-10-15 17:32:38 +0900

    update BOJ10808 (17628 kb / 164 ms)

---
+++ BOJ/src/BOJ10808/Main.java
@@ -0,0 +1,31 @@
+package BOJ10808;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] input = sc.next().toCharArray();
+
+        int[] result = new int[26];
+        int compare = 'a';
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (compare == input[j]) {
+                    result[i]++;
+                }
+            }
+
+            compare++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : result) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
