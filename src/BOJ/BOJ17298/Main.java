72063d97c52299fb45cdfd85f573f5e92d85ea2b
SON1205 <jjinoson1205@gmail.com>
2025-07-16 13:00:46 +0900

    BOJ17298 오큰수 (GOLD 4)
    구현 (321892 kb / 1840 ms)

---
+++ src/BOJ/BOJ17298/Main.java
@@ -0,0 +1,38 @@
+package BOJ.BOJ17298;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] result = new int[N];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                int idx = stack.pop();
+                result[idx] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            sb.append(num).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
