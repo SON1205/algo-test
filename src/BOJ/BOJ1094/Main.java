7f04c7b3c2099b722ec21e5d4047127a8bfc4a84
SON1205 <jjinoson1205@gmail.com>
2025-07-17 12:42:06 +0900

    BOJ1094 막대기 (SILVER 5)
    이진법 (17736 kb / 176 ms)

---
+++ src/BOJ/BOJ1094/Main.java
@@ -0,0 +1,19 @@
+package BOJ.BOJ1094;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i <= 6; i++) {
+            if ((X & (1 << i)) != 0) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
