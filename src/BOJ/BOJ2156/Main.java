508c89b3086aff34670f69b06a725eece57a89a5
SON1205 <jjinoson1205@gmail.com>
2026-01-14 10:37:10 +0900

    BOJ2156 포도주 시식 (SILVER 1)
    dynamic programming (28880 kb / 312 ms)

--- src/BOJ/BOJ2156/Main.java
+++ src/BOJ/BOJ2156/Main.java
@@ -1,37 +1,73 @@
 package BOJ.BOJ2156;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Integer> cups = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            cups.add(sc.nextInt());
+
+        int[] wine = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            wine[i] = sc.nextInt();
         }
 
-        if (n == 1 || n == 2) {
-            int total = 0;
-            for (int cup : cups) {
-                total += cup;
-            }
-            System.out.println(total);
+        if (n == 1) {
+            System.out.println(wine[1]);
+            return;
+        }
+        if (n == 2) {
+            System.out.println(wine[1] + wine[2]);
             return;
         }
 
-        int[][] dp = new int[n][2]; // 0: 선택O, 1: 선택X
-        dp[0][0] = cups.get(0);
-        dp[0][1] = 0;
-        dp[1][0] = cups.get(1) + dp[0][0];
-        dp[1][1] = dp[0][0];
-        for (int i = 2; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 2][1] + cups.get(i - 1), dp[i - 1][1]) + cups.get(i);
-            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        int[] dp = new int[n + 1];
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(
+                    dp[i - 1],
+                    Math.max(
+                            dp[i - 2] + wine[i],
+                            dp[i - 3] + wine[i - 1] + wine[i]
+                    )
+            );
         }
 
-        System.out.println(Math.max(dp[n - 1][0], dp[n - 1][1]));
+        System.out.println(dp[n]);
     }
 }
+
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        List<Integer> cups = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            cups.add(sc.nextInt());
+//        }
+//
+//        if (n == 1 || n == 2) {
+//            int total = 0;
+//            for (int cup : cups) {
+//                total += cup;
+//            }
+//            System.out.println(total);
+//            return;
+//        }
+//
+//        int[][] dp = new int[n][2]; // 0: 선택O, 1: 선택X
+//        dp[0][0] = cups.get(0);
+//        dp[0][1] = 0;
+//        dp[1][0] = cups.get(1) + dp[0][0];
+//        dp[1][1] = dp[0][0];
+//        for (int i = 2; i < n; i++) {
+//            dp[i][0] = Math.max(dp[i - 2][1] + cups.get(i - 1), dp[i - 1][1]) + cups.get(i);
+//            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+//        }
+//
+//        System.out.println(Math.max(dp[n - 1][0], dp[n - 1][1]));
+//    }
+//}
